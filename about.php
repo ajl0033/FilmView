5c57bb71cdd659ee4894d2d3214607098a02172f
Anthony Lowery <ajl0033@mix.wvu.edu>
2020-09-25 20:53:42 -0400

    Login/out & Profile

---
+++ about.php
@@ -0,0 +1,65 @@
+<?php
+require 'includes/header.php'
+?>
+<main>
+<link rel="stylesheet" href="./css/about.css" />
+    <div class="head-background">
+        <div id="top">
+            <p class="title-text"><em>Reach Out!</em></p>
+            <button> <img class="icon" src="./images/GMail.JPG" /> </button>
+            <button> <img class="icon" src="./images/Facebook.JPG" /> </button>
+            <button> <img class="icon" src="./images/Twitter.JPG" /> </button>
+            <button> <img class="icon" src="./images/Instagram.JPG" /> </button>
+            <h1>About Us</h1>
+            <p class="subtitle-text">Meet the people that make it all happen...</p>
+            <br />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <center><img src="./images/Mario.JPG" /></center>
+                <div class="container">
+                    <p>CEO</p>
+                    <p>Ability: Fireball</p>
+                    <p>email: dabestbro@yahooooo.com</p>
+                    <p>"I work here so that I can give Princess Peach the life she deserves."</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <center><img src="./images/Link.JPG" /></center>
+                <div class="container">
+                    <p>Defense Director</p>
+                    <p>Ability: Fighting</p>
+                    <p>email: mytriforce@gmail.com</p>
+                    <p>"Princess Zelda is so much better than that Peach girl Mario is with..."</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <center><img src="./images/Kirby.JPG" /></center>
+                <div class="container">
+                    <p>CFO</p>
+                    <p>Ability: Flying</p>
+                    <p>email: puffyboy@ymail.com</p>
+                    <p>"I'm thankful to have a fun job that also allows me to put cake on the table."</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <center><img src="./images/Sonic.JPG" /></center>
+                <div class="container">
+                    <p>COO</p>
+                    <p>Ability: Speed</p>
+                    <p>email: gottagofaster@outlook.com</p>
+                    <p>"I'm the most productive person here... But that's how it is when you're as fast as me."</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
--- login.php
+++ login.php
@@ -1,29 +1,12 @@
-<!DOCTYPE html>
-<html class="no-js">
+<?php
+require 'includes/header.php'
+?>
+<main>
+<link rel="stylesheet" href="css/login.css">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Login</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/login.css">
-</head>
-
-<body>
     <div class="bg-cover">
         <div class="row">
-            <div id="slides" class="carousel slide col-md-4 offset-md-4" data-ride="carousel" style="margin-top: 20px;">
+            <div id="slides" class="carousel slide col-md-4 offset-md-4" data-ride="carousel" style="margin-top: 10px;">
                 <ol class="carousel-indicators">
                   <li data-target="#slides" data-slide-to="0" class="active"></li>
                   <li data-target="#slides" data-slide-to="1"></li>
@@ -53,25 +36,23 @@
 
         <div class="h-0 center-me">
             <div class="my-auto">
-                <form class="form-signin" style="background: rgb(248, 239, 239); margin-top: -20px; border: 3px solid rgb(0, 0, 0); box-shadow: 0px 0px 20px 20px #00000086;">
+                <form class="form-signin" action="includes/login-helper.php" method="post" style="background: rgb(248, 239, 239); margin-top: -20px; border: 3px solid rgb(0, 0, 0); box-shadow: 0px 0px 20px 20px #00000086;">
                     <img src="images/icon.JPG" style="height: 59px; width: 105px; border: 2px solid black;"/>
                     <h1 class="h3 mb-3 font-weight-normal" style="padding: 10px; text-align: left;">Please sign in</h1>
-                    <label for="inputEmail" class="sr-only">Email address</label>
-                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required
+                    <label for="inputEmail" class="sr-only">Username or Email address</label>
+                    <input type="text" id="inputEmail" name="uname" class="form-control" placeholder="Username/ Email" required
                         autofocus>
                     <label for="inputPassword" class="sr-only">Password</label>
-                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                    <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Password" required>
                     <div class="checkbox mb-3" style="margin-top: 6px; text-align: left;">
                         <label>
                             <input type="checkbox" value="remember-me"> Remember me
                         </label>
                     </div>
-                    <button class="btn btn-lg btn-primary btn-block" type="submit" style="margin-top: -20px; margin-bottom: -30px;">Sign in</button>
+                    <button class="btn btn-lg btn-primary btn-block" name="login-submit" type="submit" style="margin-top: -20px; margin-bottom: -30px;">Sign in</button>
                     <p class="mt-5 mb-3 text-muted">&copy; 2020-2021</p>
                 </form>
             </div>
         </div>
     </div>
-</body>
-
-</html>
+</main>
